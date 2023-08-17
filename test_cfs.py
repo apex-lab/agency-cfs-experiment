8ab7ddc5824b946f4c5361c874ec159ee56b5293
John <johnv@uchicago.edu>
2023-08-17 14:13:05 -0500

    adds smol masked stimulus to CFS test script

--- test_cfs.py
+++ test_cfs.py
@@ -1,22 +1,35 @@
 from util.cfs import init_window, CFSMask
 from psychopy import visual
 
-win = init_window(size = (1000, 1000))
-mask = CFSMask(win, color = (1, 0, 0), size = 1.)
+SIZE = 500 # size of mask in pixels
+RED = (1, 0, 0)
+BLUE = (0, 0, 1)
+MASK_COLOR = RED
+STIM_COLOR = BLUE
 
-maximum = 60 * 10
+win = init_window(size = (2*SIZE, 2*SIZE), units = 'pix')
+mask = CFSMask(win, color = MASK_COLOR, size = SIZE)
+stim_pos = dict(
+    upper_right = (SIZE//4, SIZE//4),
+    upper_left = (-SIZE//4, SIZE//4),
+    lower_left = (-SIZE//4, -SIZE//4),
+    lower_right = (SIZE//4, -SIZE//4)
+    )
+
+
+max_frames = 60 * 10
 count = 0
 circle = visual.Circle(
     win,
-    size = 1.,
-    contrast = 1.,
-    pos = (0, 0),
-    fillColor = (0, 0, 1)
+    size = SIZE//3,
+    contrast = .3,
+    pos = stim_pos['lower_right'],
+    fillColor = STIM_COLOR
     )
 circle.autoDraw = True
 while not mask.completed:
     count += 1
-    if count < maximum:
+    if count < max_frames:
         win.callOnFlip(mask.on_flip)
     else:
         win.callOnFlip(mask.on_flip, True)

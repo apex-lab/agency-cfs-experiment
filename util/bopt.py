49ea5bb6bc7b9fe90febcb62be724240b534735d
John <johnv@uchicago.edu>
2023-09-08 09:56:39 -0500

    implements Thompson sampling strategy for QUEST

--- util/bopt.py
+++ util/bopt.py
@@ -1,4 +1,5 @@
 from psychopy.contrib.quest import QuestObject as _QuestObject
+import numpy as np
 
 class QuestObject(_QuestObject):
 
@@ -10,3 +11,23 @@ class QuestObject(_QuestObject):
         i = min(len(self.pdf), max(1,i)) - 1
         p = self.pdf[int(i)]
         return p
+
+    def draw_from_post(self):
+        '''
+        Returns a draw from the posterior distribution. Using this method
+        to pick your next sample is called "Thompson sampling," which has
+        provably nicer convergence properties than e.g. just picking the mean.
+        '''
+        p = self.pdf / self.pdf.sum()
+        return self.tGuess + np.random.choice(self.x, p = p)
+
+    def mean_exp(self, base = 10):
+        '''
+        Returns the posterior expectation of the exponentiated distribution.
+
+        This is handy when converting back from log10 scale, since
+        10**mean(x) != mean(10**x) in general.
+        '''
+        _x = self.tGuess + self.x
+        exp_x = base**_x
+        return np.sum(self.pdf * exp_x) / np.sum(self.pdf)
